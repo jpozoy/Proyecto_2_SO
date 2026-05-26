4dde2749ccab33480924c835bf2b8837b610ad37
AlbertoG52 <aguerrero@estudiantec.cr>
2026-05-25 18:40:58 -0600

    feat(Implementacion de LRU):

--- js/algorithms/ReplacementAlgorithm.js
+++ js/algorithms/ReplacementAlgorithm.js
@@ -2,12 +2,9 @@ export default class ReplacementAlgorithm {
   selectVictim(loadedPages) {
     throw new Error('selectVictim() debe implementarse en la subclase');
   }
-  // Se llama cada vez que una página es accedida (hit o recién cargada).
   notifyAccess(page) { /* sin acción */ }
 
-  // Se llama cuando una página deja la RAM (expulsión o delete/kill). 
   notifyRemove(page) { /* sin acción */ }
 
-  // Se llama una vez por operación, antes de ejecutarla. Solo el optimo lo usa. 
   notifyStep(operationIndex) { /* sin acción */ }
 }
